540d18f68a4abf80fd3d4d56ee7c8b0b01e19324
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-08-04 20:08:15 +0700

    update

---
+++ test/require/index.js
@@ -0,0 +1,8 @@
+var caculator = require("./calc");
+let x = 50;
+let y = 50;
+
+console.log(caculator.add(x, y));
+console.log(caculator.sub(x, y));
+console.log(caculator.div(x, y));
+console.log(caculator.mult(x, y));
