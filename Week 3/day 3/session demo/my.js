d81530d12c3c42c1e04dcc8f6a0a405e4879d29a
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-08-24 16:08:14 +0700

    update

--- Week 3/day 3/session demo/my.js
+++ Week 3/day 3/session demo/my.js
@@ -8,100 +8,100 @@ const mysql = require("mysql");
 const cookie = require("cookie");
 const escapeHtml = require("escape-html");
 const {type} = require("os");
+const path = [
+  "./views/home.html",
+  "./views/login.html",
+  "./views/notfound.html",
+];
+const urlName = ["/", "/login", "/notfound"];
+let listToken = [];
+const server = http.createServer((req, res) => {
+  const urlPath = url.parse(req.url);
+  const method = req.method;
+  const urlPathName = urlPath.pathname;
+  switch (urlPathName) {
+    case urlName[0]:
+      getTemplace(req, res, path[0]);
+      break;
+    case urlName[1]:
+      if (method == "GET") {
+        getTemplace(req, res, path[1]);
+      } else {
+        let data = "";
+        req.on("data", (chunk) => {
+          data += chunk;
+        });
+        req.on("end", () => {
+          let users = qs.parse(data);
+          let expires = Date.now() + 1000 * 60 * 60;
+          let tokenSession =
+            '{"name":"' +
+            users.name +
+            '","password":"' +
+            users.pass +
+            '","expires":' +
+            expires +
+            "}";
+          createTokenSession(tokenSession);
+          tokend();
+          fs.readFile(path[0], "utf-8", (err, data) => {
+            if (err) {
+              console.log(err);
+            }
+            data = data.replace("{name}", users.name);
+            data = data.replace("{pass}", users.pass);
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(data);
+            return res.end();
+          });
+        });
+      }
+      break;
+    case urlName[2]:
+      getTemplace(req, res, path[2]);
+      break;
+    default:
+      res.end();
+  }
+});
 
-let handlers = {};
-const router = {
-  login: handlers.login,
-  home: handlers.home,
-  notfound: handlers.notfound,
-};
-handlers.login = (req, res) => {
-  fs.readFile("./views/login.html", "utf-8", (err, data) => {
-    res.writeHead(200, {"content-type": "text/html"});
+server.listen(port, host);
+
+function getTemplace(req, res, pathName) {
+  fs.readFile(pathName, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.writeHead("200", {"content-type": "text/html"});
     res.write(data);
     res.end();
   });
-};
-handlers.home = (req, res) => {
-  fs.readFile("./views/home.html", "utf-8", (err, data) => {
-    res.writeHead(200, {"content-type": "text/html"});
-    res.write(data);
-    res.end();
-  });
-};
+}
 
-let createRamdomStr = (strLength) => {
+const createRandomString = function (strLength) {
   strLength =
     (typeof strLength == "number") & (strLength > 0) ? strLength : false;
   if (strLength) {
-    const possibleChar = "abcdefghiklmnopqwerszx1234567890";
-    let str = "";
+    var possibleCharacter = "abcdefghiklmnopqwerszx1234567890";
+    var str = "";
     for (let i = 0; i < strLength; i++) {
-      let randomChar = possibleChar.charAt(
-        Math.floor(Math.random() * strLength)
+      let ramdomCharater = possibleCharacter.charAt(
+        Math.floor(Math.random() * possibleCharacter.length)
       );
-      str += randomChar;
+      str += ramdomCharater;
     }
     return str;
   }
 };
-var createTokenSession = function (data) {
-  //tao ngau nhien ten file
-  let tokenId = createRamdomStr(20);
+
+const createTokenSession = function (data) {
+  let tokenId = createRandomString(20);
   let fileName = "./token/" + tokenId;
+  listToken.push(fileName);
   fs.writeFile(fileName, data, (err) => {});
 };
-
-handlers.home = function (req, res) {
-  // xu ly submit
-  var data = "";
-  req.on("data", (chunk) => {
-    data += chunk;
-  });
-  req.on("end", () => {
-    data = qs.parse(data);
-    let expires = Date.now() + 1000 * 60 * 60;
-    let tokenSession =
-      '{"name":"' +
-      data.name +
-      '","email":"' +
-      data.email +
-      '","password":"' +
-      data.password +
-      '","expires":' +
-      expires +
-      "}";
-    createTokenSession(tokenSession);
-    fs.readFile("./views/homepage.html", "utf8", function (err, datahtml) {
-      if (err) {
-        console.log(err);
-      }
-      datahtml = datahtml.replace("{name}", data.name);
-      datahtml = datahtml.replace("{email}", data.email);
-      datahtml = datahtml.replace("{password}", data.password);
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write(datahtml);
-      return res.end();
-    });
-  });
-  req.on("error", () => {
-    console.log("error");
+const tokend = () => {
+  fs.readFile(listToken[0], "utf-8", (err, data) => {
+    console.log(data);
   });
 };
-const server = http.createServer(function (req, res) {
-  var parseUrl = url.parse(req.url, true);
-  // //get the path
-
-  var path = parseUrl.pathname;
-  var trimPath = path.replace(/^\/+|\/+$/g, "");
-
-  var chosenHandler =
-    typeof router[trimPath] !== "undefined"
-      ? router[trimPath]
-      : handlers.notfound;
-  chosenHandler(req, res);
-});
-
-server.listen(8080, function () {
-  console.log("server running at localhost:8080 ");
-});
